e3b089a902b2c48cd6ee103e39cd2ab35033f5ce
Sajad Aliraqe <sajad@bareed.ws>
2016-06-20 09:32:05 +0300

    Fexd

--- plugins/upgrade.lua
+++ plugins/upgrade.lua
@@ -1,12 +1,10 @@
 
 local function run(msg, matches)
---create by RoyalTeam ID CHANNEL : @RoyalTeamCh
    if msg.to.type == 'chat' and is_momod then
 chat_upgrade('chat#id'..msg.to.id, ok_cb, false)
      return "Chat Upgraded to SuperGroups"
    end
 end
- --create by RoyalTeam ID CHANNEL : @RoyalTeamCh
  return {
    patterns = {
 "^[!/#](tosuper)$",
